077a8034ea04c4c133242fb34d84cb5f21eddea3
Isaac Sarzosa <isaac.sarzosa@gruporadical.com>
2023-07-19 17:40:09 -0500

    zzz, usar mejor bootstrap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,10 @@
 // import React from 'react';
-import '../styles/Header.css';
 import AppBar from './header/AppBar'
 
 function Header () {
   return (
     // <div className="image">
       <>
-        <header className="header">
-          <h1 className="title">Tenable Dashboard App v1.0</h1>
-          <div className="buttons">
-            <button className="login-btn">Log In</button>
-            <button className="signup-btn">Sign Up</button>
-          </div>
-        </header>
         <AppBar/>
       </>
 
